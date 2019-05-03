05b968da6c112aa8969dbb623e32371f0a407d7a
Pranay <pranay.sankpal@pharmeasy.in>
2019-05-03 13:46:25 +0530

    removed bank repository

--- src/main/java/com/fyle/bank/repositories/BankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fyle.bank.repositories;
-
-import com.fyle.bank.models.Bank;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Long> {
-}
